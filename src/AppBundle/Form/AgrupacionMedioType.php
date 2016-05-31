f25c3448c6911b6a201f042970737416ff9c0e3a
Débora <debora.vazquez@gmail.com>
2016-05-31 10:30:18 +0200

    * Modificacion textos Layout * Modificacion widget type en AgrupacionMEdio * Correccion en form de agrupacionMedio * En formulario agrupacionMedioDetalle filtrar medio por la ubicacion de la agrupacion.

--- src/AppBundle/Form/AgrupacionMedioType.php
+++ src/AppBundle/Form/AgrupacionMedioType.php
@@ -5,6 +5,7 @@ use ESocial\UtilBundle\Form\ESocialType;
 use ESocial\UtilBundle\Form\Widget\SelectableEntityType;
 use ESocial\UtilBundle\Util\Dates;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -20,18 +21,17 @@ class AgrupacionMedioType extends ESocialType
         $data = array_key_exists('data', $options) ? $options['data'] : null;
 
         $builder
-            ->add('descripcion', null, array('label' => 'form.agrupacion_medio.label.descripcion'));
-
-        if ($data && !$data->getUbicacion()) {
-            $builder->add('ubicacion', SelectableEntityType::class, array('label' => 'form.agrupacion_medio.label.ubicacion', 'class' => 'VallasModelBundle:Ubicacion',
+            ->add('descripcion', null, array('label' => 'form.agrupacion_medio.label.descripcion'))
+            ->add('tipo', ChoiceType::class, array('label' => 'form.agrupacion_medio.label.tipo', 'placeholder' => 'form.label.choice_empty_value', 'choices' => $this->sf3TransformChoiceOptions(array(
+                '0' => 'Landmark',
+                '1' => 'Maxivalla Doble'
+            ))))
+            ->add('coste', null, array('label' => 'form.agrupacion_medio.label.coste'))
+            ->add('ubicacion', SelectableEntityType::class, array('label' => 'form.agrupacion_medio.label.ubicacion', 'class' => 'VallasModelBundle:Ubicacion',
                 'required' => true,
                 'select_text' => 'Select Ubicación',
-                'enable_update' => true));
-        }
-
-        $builder
-            ->add('tipo', null, array('label' => 'form.agrupacion_medio.label.tipo'))
-            ->add('coste', null, array('label' => 'form.agrupacion_medio.label.coste'));
+                'enable_update' => true, 'field_name' => 'pk_ubicacion'));
+        ;
     }
 
     public function getName()
